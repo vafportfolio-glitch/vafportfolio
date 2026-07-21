8a8af8e481673ece416df49a728296c8d922a4a5
Usairam-s <muhammaduseram@gmail.com>
2026-07-22 02:32:06 +0500

    hello founder out

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <TypingHero />
-      <MeetFounder />
+      {/* <MeetFounder /> */}
       <PortfolioProjects />
       <TechStack />
       <Testimonials />
